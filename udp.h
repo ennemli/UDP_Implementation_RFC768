a4c8c7a000f7a982c7f9c7a6048ad8861152401d
Mohsine Ennemli <ennemli.moh55@gmail.com>
2025-02-08 03:54:51 +0100

    updated udp.h

--- udp.h
+++ udp.h
@@ -4,15 +4,25 @@
 #include "unistd.h"
 #include <cstdint>
 #include <sys/types.h>
+#include <vector>
 
-struct UDP_HEADER {
-  uint16_t sourcePort;
-  uint16_t destPort;
-  uint16_t lenght;
-  uint16_t checksum;
+struct UDPPacket {
+  struct PseudoHeader {
+    uint32_t src_ip;
+    uint32_t dest_ip;
+    uint8_t zero;
+    uint8_t protocol;
+    uint8_t udpLength;
+  } pseudo_header;
+  struct UDPHeader {
+    uint16_t sourcePort;
+    uint16_t destPort;
+    uint16_t lenght;
+    uint16_t checksum;
+  } udp_header;
+  std::vector<uint8_t> payload;
 };
 
-uint16_t calculate_checksum(const UDP_HEADER *udpHeader, const char *data,
-                            size_t dataLen);
+uint16_t calculate_checksum(const UDPPacket &udpPacket);
 
 #endif // !UDP_H
